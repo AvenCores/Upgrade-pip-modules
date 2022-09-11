c53cb148ea360e1150e05714f5ff3d75a4982343
AvenCores <avencores@gmail.com>
2022-09-11 12:27:03 +0300

    Code optimization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from os import system
 from sys import platform
-import pkg_resources
+from pkg_resources import working_set
 from subprocess import call
 
 def clear():
@@ -17,7 +17,7 @@ system("pip3 install --upgrade pip")
 if platform == "linux" or platform == "linux2" or platform == "unix":
 	system("for i in $(pip list -o | awk 'NR > 2 {print $1}'); do pip install -U $i; done")
 elif platform == "win32":
-	packages = [dist.project_name for dist in pkg_resources.working_set]
+	packages = [dist.project_name for dist in working_set]
 	call("pip install --upgrade " + ' '.join(packages), shell=True)
 else:
 	print("Неподдерживаемая система!")
